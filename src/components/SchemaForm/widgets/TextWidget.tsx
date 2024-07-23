99d53fe97cda138a0cd7eed26509517fd5405c0a
Pásztor Gábor <gpasztor87@users.noreply.github.com>
2024-07-23 14:57:15 +0200

    Update TextWidget.tsx

--- src/components/SchemaForm/widgets/TextWidget.tsx
+++ src/components/SchemaForm/widgets/TextWidget.tsx
@@ -1,11 +1,11 @@
-import { ChangeEvent, useCallback } from "react";
+import { ChangeEvent, FocusEvent, useCallback } from "react";
 
 import { WidgetProps, getInputProps } from "@rjsf/utils";
 
 import { Input } from "@/components/ui/input";
 
 export function TextWidget(props: WidgetProps) {
-  const { id, name, onChange, schema, options } = props;
+  const { id, name, onBlur, onChange, onFocus, schema, options } = props;
   const inputProps = getInputProps(schema, undefined, options);
 
   const handleChange = useCallback(
@@ -14,5 +14,26 @@ export function TextWidget(props: WidgetProps) {
     [onChange, options],
   );
 
-  return <Input id={id} name={name} {...inputProps} onChange={handleChange} />;
+  const handleBlur = useCallback(
+    ({ target }: FocusEvent<HTMLInputElement>) =>
+      onBlur(id, target && target.value),
+    [onBlur, id],
+  );
+
+  const handleFocus = useCallback(
+    ({ target }: FocusEvent<HTMLInputElement>) =>
+      onFocus(id, target && target.value),
+    [onFocus, id],
+  );
+
+  return (
+    <Input
+      id={id}
+      name={name}
+      {...inputProps}
+      onBlur={handleBlur}
+      onChange={handleChange}
+      onFocus={handleFocus}
+    />
+  );
 }
