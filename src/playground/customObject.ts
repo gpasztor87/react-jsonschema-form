3f91736b172dfd81a384e59343af56c17465b547
Pásztor Gábor <gpasztor87@users.noreply.github.com>
2024-07-26 13:30:49 +0200

    Add more examples

---
+++ src/playground/customObject.ts
@@ -0,0 +1,44 @@
+export default {
+  schema: {
+    title: "A registration form",
+    description:
+      "This is the same as the simple form, but with an altered bootstrap grid. Set the theme to default, and try shrinking the browser window to see it in action.",
+    type: "object",
+    required: ["firstName", "lastName"],
+    properties: {
+      firstName: {
+        type: "string",
+        title: "First name",
+      },
+      lastName: {
+        type: "string",
+        title: "Last name",
+      },
+      age: {
+        type: "integer",
+        title: "Age",
+      },
+      bio: {
+        type: "string",
+        title: "Bio",
+      },
+      password: {
+        type: "string",
+        title: "Password",
+        minLength: 3,
+      },
+      telephone: {
+        type: "string",
+        title: "Telephone",
+        minLength: 10,
+      },
+    },
+  },
+  formData: {
+    firstName: "Chuck",
+    lastName: "Norris",
+    age: 75,
+    bio: "Roundhouse kicking asses since 1940",
+    password: "noneed",
+  },
+};
--- src/playground/index.ts
+++ src/playground/index.ts
@@ -1,14 +1,26 @@
+import additionalProperties from "./additionalProperties";
 import customArray from "./customArray";
+import customObject from "./customObject";
+import errors from "./errors";
+import ifThenElse from "./ifThenElse";
+import nested from "./nested";
 import nullable from "./nullable";
+import propertyDependencies from "./propertyDependencies";
 import schemaDependencies from "./schemaDependencies";
 import simple from "./simple";
 import single from "./single";
 
 export const samples = Object.freeze({
   Simple: simple,
+  Nested: nested,
   Single: single,
+  Errors: errors,
+  "Custom Object": customObject,
   "Custom Array": customArray,
+  "Additional Properties": additionalProperties,
+  "Property dependencies": propertyDependencies,
   "Schema dependencies": schemaDependencies,
+  "If Then Else": ifThenElse,
   Nullable: nullable,
 } as const);
 
