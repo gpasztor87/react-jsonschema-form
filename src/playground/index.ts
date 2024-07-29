cd76a62cd7a67538d5c0afabb55461b2f21496ef
Pásztor Gábor <gpasztor87@users.noreply.github.com>
2024-07-29 23:19:16 +0200

    Add ore examples

--- src/playground/index.ts
+++ src/playground/index.ts
@@ -1,12 +1,16 @@
 import additionalProperties from "./additionalProperties";
+import arrays from "./arrays";
 import customArray from "./customArray";
 import customObject from "./customObject";
+import defaults from "./defaults";
 import errors from "./errors";
 import ifThenElse from "./ifThenElse";
 import nested from "./nested";
 import nullable from "./nullable";
 import numbers from "./numbers";
+import ordering from "./ordering";
 import propertyDependencies from "./propertyDependencies";
+import references from "./references";
 import schemaDependencies from "./schemaDependencies";
 import simple from "./simple";
 import single from "./single";
@@ -14,7 +18,10 @@ import single from "./single";
 export const samples = Object.freeze({
   Simple: simple,
   Nested: nested,
+  Arrays: arrays,
   Numbers: numbers,
+  Ordering: ordering,
+  References: references,
   Single: single,
   Errors: errors,
   "Custom Object": customObject,
@@ -24,6 +31,7 @@ export const samples = Object.freeze({
   "Schema dependencies": schemaDependencies,
   "If Then Else": ifThenElse,
   Nullable: nullable,
+  Defaults: defaults,
 } as const);
 
 export type Sample = keyof typeof samples;
